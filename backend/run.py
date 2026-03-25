a83a87880318c8025c0b779ccd7c6afbf4fd4e02
Wise Munene Mutuku <wisemunene99@gmail.com>
2026-03-25 14:13:49 +0300

    port issue

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,8 @@
+import os
 from app import create_app
 
 app = create_app()
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host= '0.0.0.0', port=int(os.environ.get('PORT', 10000)))
