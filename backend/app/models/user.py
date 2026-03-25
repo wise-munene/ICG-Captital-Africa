33b0172106c692b003e6804129c82f1555293aa5
Wise Munene Mutuku <wisemunene99@gmail.com>
2026-03-25 15:07:32 +0300

    fixing users table, create tables

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -5,6 +5,7 @@ from ..extensions import db
 from sqlalchemy.dialects.postgresql import UUID
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
