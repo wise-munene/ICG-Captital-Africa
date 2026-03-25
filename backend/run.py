c24328b1699aed6490f5a3ea21fa8bce92ca9b97
Wise Munene Mutuku <wisemunene99@gmail.com>
2026-03-25 15:24:10 +0300

    fix seed db

--- backend/run.py
+++ backend/run.py
@@ -1,11 +1,22 @@
 import os
 from app import create_app
 from app.extensions import db
+from app.seeds.seed_services import seed_services
+from app.seeds.seed_users import seed_users
+from app.seeds.seed_settings import seed_settings   
 
 app = create_app()
 
 with app.app_context():
     db.create_all()  # Create tables if they don't exist
+
+    try:
+        seed_services()
+        seed_users()
+        seed_settings()
+        print("Database seeding complete.")
+    except Exception as e:
+        pass
 
 
 if __name__ == '__main__':
