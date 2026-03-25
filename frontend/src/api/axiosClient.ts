7c7f26adf26d5841c5e748e14c6dbe8c62019c10
Wise Munene Mutuku <wisemunene99@gmail.com>
2026-03-25 21:55:46 +0300

    fix api url

--- frontend/src/api/axiosClient.ts
+++ frontend/src/api/axiosClient.ts
@@ -1,9 +1,12 @@
-//creating the api connection using axios, which is a popular HTTP client for making requests to the backend API. The axiosClient instance is configured with a base URL and default headers, making it easier to make API calls throughout the application without having to specify the base URL and headers each time.
-
 import axios from 'axios';
 
-const api = axios.create({  //create an instance of axios with default configuration
-  baseURL: "https://icg-backend-cppd.onrender.com/api"  //set the base URL for API requests, using an environment variable or defaulting to localhost
+const baseURL = import.meta.env.VITE_API_URL ||
+ 'http://icg-backend-cppd.onrender.com/api';
+
+ console.log("API Base URL:", baseURL);  // Log the base URL to verify it's being read correctly
+
+const axiosClient = axios.create({
+  baseURL,
 });
 
-export default api;  //export the configured axios instance for use in other parts of the application
+export default axiosClient;
