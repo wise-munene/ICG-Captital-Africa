c10821be360e63037b03566135643639576e3087
Wise Munene Mutuku <wisemunene99@gmail.com>
2026-03-25 21:17:40 +0300

    correct api endpoint for services

--- frontend/src/services/serviceAPI.ts
+++ frontend/src/services/serviceAPI.ts
@@ -3,6 +3,6 @@
 import axiosClient from "../api/axiosClient";
 
 export const getServices = async () => {  // Replace with your actual API endpoint
-    const resonse = await axiosClient.get('/services/');  // Replace with your actual API endpoint
-    return resonse.data;
+    const response = await axiosClient.get('/services');  // Replace with your actual API endpoint
+    return response.data;
 }
